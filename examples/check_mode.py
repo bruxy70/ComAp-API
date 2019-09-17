b2f83e22f3734ece9c9e307c6f10dd113f5c07ae
bruxy70 <vasek.chaloupka@hotmail.com>
2019-09-17 15:27:44 +0200

    Added comm state

--- examples/check_mode.py
+++ examples/check_mode.py
@@ -18,6 +18,7 @@ logging.basicConfig(level=logging.CRITICAL)
 VALUE_GUID = {
     "engine_state": "BB2D1ADE-740E-488d-853B-6BA970D52E27",
     "mode":         "6a12aed6-3be0-44c4-9110-9ea33cfe3ccc",
+    "state":        "F0219C1C-1860-4b4d-8E6E-3CEC96279D6F",
     "fuel_level":   "0dc2739f-0fc9-4a17-bc91-26e5dda19ed8"
 }
 
@@ -25,10 +26,14 @@ async def check_status():
     session=aiohttp.ClientSession(raise_for_status=True)
     wsv=comapapi_async(session,KEY,TOKEN)
     units = await wsv.async_units()
+    print(f'{"Name":>35}  {"State":<12} {"Mode":<5} Since')
+    print('---------------------------------------------------------------------------')
     for unit in units:
-        values=await wsv.async_values(unit["unitGuid"],VALUE_GUID["mode"].lower())
-        if len(values)>0:
-            print(f'{unit["name"]:>35}  {values[0]["value"]:<5} since {values[0]["timeStamp"]}')
+        values=await wsv.async_values(unit["unitGuid"],f'{VALUE_GUID["state"]},{VALUE_GUID["mode"]}')
+        if len(values)==1:
+            print(f'{unit["name"]:>35}  {values[0]["value"]:<12} N/A')
+        elif len(values)==2:
+            print(f'{unit["name"]:>35}  {values[0]["value"]:<12} {values[1]["value"]:<5} {values[1]["timeStamp"]}')
         else:
             print(f'{unit["name"]:>35}  N/A')
     await session.close()
