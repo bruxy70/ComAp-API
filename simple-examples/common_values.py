7646f3ee7b1e077bb07d50fb917edead9c5c665e
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-17 19:35:02 +0200

    Update common_values.py

--- simple-examples/common_values.py
+++ simple-examples/common_values.py
@@ -1,5 +1,6 @@
 from comap.constants import VALUE_GUID
 
 print("These common values GUIDs are stored in the constant VALUE_GUID[value], available in module comap.constants.")
+print("------------------------------------------------------------------------------------------------------------")
 for value in VALUE_GUID:
-    print(value)
+    print(f'{value:<16} {VALUE_GUID[value]}')
