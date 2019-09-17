b948d80bb19fd32515f9671e2b997a2c268ea50f
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-17 19:07:03 +0200

    Common values stored in connstant VALUE_GUID

---
+++ simple-examples/common_values.py
@@ -0,0 +1,5 @@
+from comap.constants import VALUE_GUID
+
+print("These common values GUIDs are stored in the constant VALUE_GUID[value], available in module comap.constants.")
+for value in VALUE_GUID:
+    print(value)
