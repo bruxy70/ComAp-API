04ec99950889d1ffd0141aa70e95a1f5b46d6849
bruxy70 <vasek.chaloupka@hotmail.com>
2023-01-05 18:27:45 +0100

    black

--- examples/asynchronous/archive_files.py
+++ examples/asynchronous/archive_files.py
@@ -3,16 +3,18 @@ From all controllers registered under the user account, archive the files yanger
 To run: python archive_files.py <age> (e.g. 'python archive_files.py 7' for the backup of last 7 days)
 Age is a number of days and is optional. If no age is given, the script will prompt
 """
+import asyncio
 import logging
 import os
 import sys
-import asyncio
-import logging
+from datetime import date, datetime
+
 import aiohttp
-from config import KEY, TOKEN
-from datetime import datetime, date
 from comap.api_async import wsv_async
+from config import KEY, TOKEN
+
 logging.basicConfig(level=logging.ERROR)
+
 
 async def backup(age):
     session = aiohttp.ClientSession(raise_for_status=True)
@@ -20,14 +22,19 @@ async def backup(age):
     units = await wsv.async_units()
     for unit in units:
         files = await wsv.async_files(unit["unitGuid"])
-        print(f'Archiving {unit["name"]}...')    
-        for file in list(filter(lambda f: (today - f["generated"].date()).days <= age, files)):
+        print(f'Archiving {unit["name"]}...')
+        for file in list(
+            filter(lambda f: (today - f["generated"].date()).days <= age, files)
+        ):
             print(f'Downloading file {file["fileName"]}')
             if not os.path.exists(unit["name"]):
                 os.mkdir(unit["name"])
-            downloaded = await wsv.async_download(unit["unitGuid"], file["fileName"], unit["name"])
+            downloaded = await wsv.async_download(
+                unit["unitGuid"], file["fileName"], unit["name"]
+            )
             print(f"{' - SUCCESS' if downloaded else ' - FAILED'}")
     await session.close()
+
 
 today = datetime.now().date()
 try:
